b0d9cce0689811bccfaf07b15d6913faebea2001
LArchaon <alex.buzdin@gmail.com>
2014-04-09 11:48:51 +0300

    extracted delimiter table

---
+++ src/main/groovy/tsi/compilers/LexicalAnalyser.groovy
@@ -0,0 +1,24 @@
+package tsi.compilers
+
+class LexicalAnalyser {
+    private char currentChar;
+    private int currentCharClass;
+    private String lexem = "";
+    private String j;
+
+    private void init() {
+
+    }
+
+    private int lookupLexem(String lexem) {
+
+    }
+
+    private char scanChar() {
+        return '';
+    }
+
+    private boolean isBlank(char c) {
+        return false;
+    }
+}
